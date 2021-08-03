1f30e8a8806aa783d30ee3c011be89f552502a3f
IRRathi <rathiir113@gmail.com>
2021-08-03 10:06:08 +0530

    Password functionality

---
+++ src/test/java/org/login/Login.java
@@ -0,0 +1,13 @@
+package org.login;
+
+public class Login {
+
+	public static void password() {
+		// TODO Auto-generated method stub
+System.out.println("password ");
+	}
+
+public static void main(String[] args) {
+	password();
+}
+}
